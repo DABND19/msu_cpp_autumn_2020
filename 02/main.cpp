c697c8296b9be19ec61ae85e0b564337c78f459a
DABND19 <grigorenko.david@outlook.com>
2020-10-16 16:03:31 +0300

    Добавил новые тесты

--- 02/main.cpp
+++ 02/main.cpp
@@ -84,11 +84,41 @@ void TestTokenTypesAndChangeTriggers()
   std::cerr << "Test change triggers is done!" << std::endl;
 }
 
+void TestChangeInputs()
+{
+  const std::string input1 = "a b c d e f 1 2 3 4 5 6";
+  const std::string input2 = "g h i j k l 3 4 5 6 7";
+  std::stringstream output;
+
+  TokenParser p;
+
+  p.setStringTrigger([&output](const std::string& word) {
+    output << word << " ";
+  });
+
+  p.setNumberTrigger([&output](int64_t number) {
+    output << number << " ";
+  });
+
+  p.runParser(input1);
+
+  assert(output.str() == (input1 + " "));
+
+  output = std::stringstream("");
+
+  p.runParser(input2);
+
+  assert(output.str() == (input2 + " "));
+
+  std::cerr << "Test change inputs is done!" << std::endl;
+}
+
 void RunTests()
 {
   TestWithoutTriggers();
   TestEmptyString();
   TestTokenTypesAndChangeTriggers();
+  TestChangeInputs();
 }
 
 int main()
