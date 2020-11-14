9308ad453c9ecc3896700a33adc7339e900677e5
DABND <grigorenko.david@outlook.com>
2020-11-14 21:41:35 +0300

    Дз 6 готово

---
+++ 06/format.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <vector>
+
+size_t toSize_t(std::string_view num) {
+  size_t result = 0;
+
+  if (num.empty()) {
+    throw std::runtime_error("can't convert index.");
+  }
+
+  for (auto digit : num) {
+    if (!isdigit(digit)) {
+      throw std::runtime_error("can't convert index.");
+    }
+
+    result = result * 10 + (digit - '0');
+  }
+
+  return result;
+}
+
+template <typename T>
+std::string toString(const T& object) {
+  std::stringstream result;
+  result << object;
+  return result.str();
+}
+
+template <typename... ArgsT>
+std::string format(std::string_view str, const ArgsT&... args) {
+  std::stringstream result;
+  std::vector<std::string> arg = {toString(args)...};
+
+  while (!str.empty()) {
+    auto left = str.find('{');
+    result << str.substr(0, left);
+    if (left == str.npos) {
+      break;
+    }
+
+    auto right = str.find('}');
+    if (right == str.npos || right < left) {
+      throw std::runtime_error("invalid scope seq.");
+    }
+
+    auto index_view = str.substr(left + 1, right - (left + 1));
+    try {
+      result << arg.at(toSize_t(index_view));
+    } catch (const std::out_of_range&) {
+      throw std::runtime_error("invalid args seq.");
+    }
+
+    str.remove_prefix(right + 1);
+  }
+
+  return result.str();
+}
---
+++ 06/main.cpp
@@ -0,0 +1,90 @@
+#include <cmath>
+#include <iostream>
+
+#include "format.hpp"
+#include "test_runner.h"
+
+using namespace std;
+
+void BasicTests() {
+  // your test
+  {
+    auto text = format("{1} + {1} = {0}", 2, "one");
+    ASSERT_EQUAL(text, "one + one = 2")
+  }
+
+  // without formatting
+  {
+    auto text = format("Hello, World!!!", "Goodbye, Cruel World!!!");
+    ASSERT_EQUAL(text, "Hello, World!!!")
+  }
+}
+
+void ExceptionsTests() {
+  // too many args
+  {
+    bool is_catched = false;
+    try {
+      auto text = format("{1} + {1} = {3}", 2, "one");
+    } catch (const std::runtime_error&) {
+      is_catched = true;
+    }
+    ASSERT(is_catched)
+  }
+
+  // invalid scope sequnce 1
+  {
+    bool is_catched = false;
+    try {
+      auto text = format("{1} + {1} = {", 2, "one");
+    } catch (const std::runtime_error&) {
+      is_catched = true;
+    }
+    ASSERT(is_catched)
+  }
+
+  // empty index
+  {
+    bool is_catched = false;
+    try {
+      auto text = format("{1} + {1} = {}", 2, "one");
+    } catch (const std::runtime_error&) {
+      is_catched = true;
+    }
+    ASSERT(is_catched)
+  }
+
+  // invalid index
+  {
+    bool is_catched = false;
+    try {
+      auto text = format("{1.5} + {1} = {0}", 2, "one");
+    } catch (const std::runtime_error&) {
+      is_catched = true;
+    }
+    ASSERT(is_catched)
+  }
+
+  // invalid scope sequnce 2
+  {
+    bool is_catched = false;
+    try {
+      auto text = format("{1} + 1} = {0}", 2, "one");
+    } catch (const std::runtime_error&) {
+      is_catched = true;
+    }
+    ASSERT(is_catched)
+  }
+}
+
+void RunTests() {
+  TestRunner tr;
+  RUN_TEST(tr, BasicTests);
+  RUN_TEST(tr, ExceptionsTests);
+}
+
+int main() {
+  RunTests();
+
+  return 0;
+}
